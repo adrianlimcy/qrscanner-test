f15f911f1f048bbda5a424396845aed0ca183b6d
ADRIAN LIM <adrianlim@ADRIANs-MacBook-Pro-2.local>
2020-08-07 21:39:03 +0800

    added some css to control transparency issues

--- README.md
+++ README.md
@@ -5,4 +5,14 @@ mobile qr scanning app
 # Steps
 1. quasar dev -m android
     - this will ask to install the quasar cordova, go ahead and install it
+2. install the cordova plugins
+    - cd src-cordova
+    - cordova plugin add cordova-plugin-qrscanner
 
+
+
+# references
+1. https://stackoverflow.com/questions/43225506/cordova-cordova-plugin-qrscanner-opaque-camera-view
+2. https://medium.com/@yangnana11/cordova-plugin-qrscanner-getting-started-df747718f2e
+3. https://github.com/bitpay/cordova-plugin-qrscanner
+4. https://www.learningsomethingnew.com/how-to-make-an-image-uploading-app-with-vue-quasar-firebase-storage-and-cordova-part-1
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,89 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page :class="ready ? 'flex flex-center bg-white' : 'flex flex-center'">
+    <div v-if="ready">
+      {{imageURI}}
+    </div>
+    <div>
+      <button v-if="authorized" class="secondary push" @click="goScan()">Go Scan</button>
+    </div>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  mounted () {
+    this.prepDevice()
+  },
+  data () {
+    return {
+      ready: true,
+      imageURI: '',
+      authorized: false,
+      selection: 'standard',
+      selectOptions: [
+        {
+          label: 'Camera-thumbnail',
+          value: 'camera-thmb'
+        },
+        {
+          label: 'Standard',
+          value: 'standard'
+        }
+      ],
+      enableVisibility: 'hidden',
+      backColor: 'transparent'
+    }
+  },
+  methods: {
+    prepDevice () {
+      window.QRScanner.prepare(this.onDone)
+    },
+    onDone: function (err, status) {
+      if (err) {
+        alert('preparing: error code = ' + err.code)
+      }
+      if (status.authorized) {
+        this.authorized = true
+      } else if (status.denied || !status.authorized) {
+        this.openSettings()
+      } else {
+
+      }
+    },
+    goScan: function () {
+      this.authorized = false
+      this.ready = false
+
+      window.QRScanner.show()
+
+      window.QRScanner.scan(this.displayContents)
+    },
+    displayContents: function (err, text) {
+      if (err) {
+        alert('scanning: error code = ' + err.code)
+        if (err.name === 'SCAN_CANCELED') {
+          alert('The scan was cancelled before a QR code was found.')
+        }
+      } else {
+        alert(text)
+        this.imageURI = text
+      }
+      window.QRScanner.destroy()
+      this.authorized = true
+      this.ready = true
+    },
+    cancelScan () {
+      window.QRScanner.cancelScan()
+      this.authorized = true
+    },
+    openSettings () {
+      if (status.canOpenSettings) {
+        if (confirm('Would you like to enable QR code scanning? You can allow camera access in your settings.')) {
+          window.QRScanner.openSettings()
+        }
+      }
+    }
+  }
 }
 </script>
